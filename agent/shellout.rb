383d6d82b366e11524d82bcf33afda3d55331d71
Nathan Powell <nathan@nathanpowell.org>
2012-02-05 19:47:01 -0500

    typo/cleanup

--- agent/shellout.rb
+++ agent/shellout.rb
@@ -1,5 +1,5 @@
 # Don't be an idiot.  If you deploy this agent to your servers use ACLs at the MQ level and make 
-# DAMN SURE your mco host only allows trusted senior people senior authentication and sudo access
+# DAMN SURE your mco host only allows trusted senior people to authenticate and sudo access
 
 # There can be no shell safe checks here lest we make the agent useless.  YOU HAVE BEEN WARNED!
 module MCollective
@@ -8,7 +8,6 @@ module MCollective
       action "so" do 
 	validate :msg, String
         run("#{request[:msg]}", :stdout => :out, :stderr => :err, :chomp => true)
-        reply[:out]
       end
     end
   end
