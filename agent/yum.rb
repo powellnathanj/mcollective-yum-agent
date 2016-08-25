a335c789f42f209ff20d13c3df03c7e536d26ace
Robin Bowes <robin.bowes@yo61.com>
2016-08-25 16:51:40 +0100

    First attempt at a standalone mco application

--- agent/yum.rb
+++ agent/yum.rb
@@ -1,33 +1,33 @@
 # Most of this agent was taken, verbatim, from the Packages agent.
-# Rather than muddy the waters by forking that agent I created a 
+# Rather than muddy the waters by forking that agent I created a
 # separate agent that is ONLY concerned with Yum...(I live in an Ivory Tower
 # where I only need to worry about RHEL)
 
 # This also needs to be a bit more transparent to what's going on and handling failures
 module MCollective
   module Agent
-    class Yum<RPC::Agent
-      metadata :name      => "Yum Agent",
-       :description       => "This is an agent for invoking yum actions on nodes",
-       :author            => "Nathan Powell <nathan@nathanpowell.org>",
-       :liberal_borrowing => "From here:  https://github.com/puppetlabs/mcollective-plugins/tree/master/agent/package/agent",
-       :license           => "Apache License, Version 2.0",
-       :version           => "1.0",
-       :url               => "http://nathanpowell.org/",
-       :timeout           => 600
+    class Yum < RPC::Agent
 
-      ["install", "downgrade", "remove", "reinstall"].each do |act|
-        action act do
-          validate :package, :shellsafe
-          do_yum_action(request[:package], act.to_sym)
+      metadata name:              'Yum Agent',
+               description:       'This is an agent for invoking yum actions on nodes',
+               author:            'Nathan Powell <nathan@nathanpowell.org>',
+               liberal_borrowing: 'From here:  https://github.com/puppetlabs/mcollective-plugins/tree/master/agent/package/agent',
+               license:           'Apache License, Version 2.0',
+               version:           '1.0',
+               url:               'http://nathanpowell.org/',
+               timeout:           600
+
+      %w(install downgrade reinstall remove update update-minimal).each do |command|
+        action command do
+          do_yum_command(command, request.data)
         end
       end
 
-      action "list" do
+      action 'list' do
         check_for_yum
 
-        valid_options = ['installed', 'all', 'available', 'extras', 'obsoletes', 'updates']
-        args = ""
+        valid_options = %w(installed all available extras obsoletes updates)
+        args = ''
 
         if request[:option]
           if valid_options.include? request[:option]
@@ -37,47 +37,36 @@ module MCollective
           end
         end
 
-        if request[:packages]
-          validate :packages, :shellsafe
-          args << " #{request.data[:packages]} "
-        end
-
-        reply[:exitcode] = run("/usr/bin/yum list #{args}", :stdout => :output, :chomp => true)
+        reply[:exitcode] = run("/usr/bin/yum -q list #{args} #{request.data[:packages]}", stdout: :output, chomp: true)
       end
 
       # Not sure how I feel about this yet, but I do want the functionality
-      action "update" do
-        check_for_yum
-        if request[:package]
-          validate :package, :shellsafe
-          do_yum_action(request[:package], :update)
-        else
-          # This is still dangerous, if you accidently misspell `package=...`
-          # it will default to `yum update -y` but there is no nice way around
-          # this: https://tickets.puppetlabs.com/browse/MCO-55
-          reply[:exitcode] = run("/usr/bin/yum update -y", :stdout => :output, :chomp => true)
-        end
+      action 'update' do
+        do_yum_command('update', request.data)
       end
 
-      # If you install the yum downloadonly plugin this can speed up patching by allowing you to 
+      # If you install the yum downloadonly plugin this can speed up patching by allowing you to
       # pre-stage patches before your window.
-      action "downloadonly" do
+      action 'downloadonly' do
         check_for_yum
-        reply.fail! "downloadonly plugin not found!" unless File.exist?("/usr/lib/yum-plugins/downloadonly.py")
+        reply.fail! 'downloadonly plugin not found!' unless File.exist?('/usr/lib/yum-plugins/downloadonly.py')
         if request[:package]
           validate :package, :shellsafe
-          reply[:exitcode] = run("/usr/bin/yum install #{request[:package]} -y --downloadonly", :stdout => :output, :chomp => true)
+          reply[:exitcode] = run("/usr/bin/yum install #{request[:package]} -y --downloadonly", stdout: :output, chomp: true)
         else
-          reply[:exitcode] = run("/usr/bin/yum update -y --downloadonly", :stdout => :output, :chomp => true)
+          reply[:exitcode] = run('/usr/bin/yum update -y --downloadonly', stdout: :output, chomp: true)
         end
       end
 
       # https://github.com/slaney/mcollective-yum-agent/pull/4
-      ["check_update", "check-update"].each do |act|
+      ['check_update', 'check-update'].each do |act|
         action act do
           check_for_yum
-
-          reply[:exitcode] = run("/usr/bin/yum -q check-update", :stdout => :output, :chomp => true)
+          if request.data.has_key? :packages
+            validate :packages, :shellsafe
+          end
+          args = generate_args request.data
+          reply[:exitcode] = run("/usr/bin/yum -q check-update #{args} #{request.data[:packages]}", stdout: :output, chomp: true)
 
           if reply[:exitcode].zero?
             reply[:outdated_packages] = []
@@ -90,17 +79,17 @@ module MCollective
         end
       end
 
-      action "clean" do
+      action 'clean' do
         check_for_yum
 
         clean_mode = if request[:mode]
                        request[:mode]
                      else
-                       @config.pluginconf["package.yum_clean_mode"] || "all"
+                       @config.pluginconf['package.yum_clean_mode'] || 'all'
                      end
 
-        if ["all", "headers", "packages", "metadata", "dbcache", "plugins", "expire-cache"].include?(clean_mode)
-            reply[:exitcode] = run("/usr/bin/yum clean #{clean_mode}", :stdout => :output, :chomp => true)
+        if ['all', 'headers', 'packages', 'metadata', 'dbcache', 'plugins', 'expire-cache'].include?(clean_mode)
+          reply[:exitcode] = run("/usr/bin/yum clean #{clean_mode}", stdout: :output, chomp: true)
         else
           reply.fail! "Unsupported yum clean mode: #{clean_mode}"
         end
@@ -108,16 +97,32 @@ module MCollective
         reply.fail! "Yum clean failed, exit code was #{reply[:exitcode]}" unless reply[:exitcode].zero?
       end
 
-      # Helper methods
-      private
-      def check_for_yum
-        reply.fail! "Cannot find yum at /usr/bin/yum" unless File.exist?("/usr/bin/yum")
+    # Helper methods
+    private
+      # mak
+      def generate_args(request_data)
+        args = ''
+        args = ' --security' if request_data.has_key? :security
+        args = " #{args} --bugfixes" if request_data.has_key? :bugfixes
+        if request_data.has_key? :cve
+          args = " %{args} " + request_data[:cve].split.map{|item| "--cve #{item}"}.join(' ')
+        end
+        if request_data.has_key? :bz
+          args = " %{args} " + request_data[:bz].split.map{|item| "--bz #{item}"}.join(' ')
+        end
+        if request_data.has_key? :'sec-severity'
+          args = " %{args} " + request_data[:'sec-severity'].split.map{|item| "--sec-severirt #{item}"}.join(' ')
+        end
+        args
       end
 
-      def do_yum_action(package, action)
-        check_for_yum
+      def check_for_yum
+        reply.fail! 'Cannot find yum at /usr/bin/yum' unless File.exist?('/usr/bin/yum')
+      end
 
-        reply[:exitcode] = run("/usr/bin/yum #{action} #{package} -y", :stdout => :output, :chomp => true)
+      def do_yum_command(command, request_data)
+        check_for_yum
+        reply[:exitcode] = run("/usr/bin/yum -q #{command} #{request_data[:packages]} -y #{generate_args request_data}", stdout: :output, chomp: true)
       end
 
       def do_outdated_packages(packages)
@@ -129,18 +134,17 @@ module MCollective
                            end
         cleaned_packages.scan(/\s*(\S+)\s*(\S+)\s*(\S+)/).each do |package|
           pkg, ver, repo = package
-          if pkg && ver && repo
-            pkginfo = {
-              :package => pkg.strip,
-              :version => ver.strip,
-              :repo => repo.strip
-            }
-            outdated_pkgs << pkginfo
-          end
+          next unless pkg && ver && repo
+          pkginfo = {
+            package: pkg.strip,
+            version: ver.strip,
+            repo:    repo.strip
+          }
+          outdated_pkgs << pkginfo
         end
         outdated_pkgs
       end
+
     end
   end
 end
-
--- application/yum.rb
+++ application/yum.rb
@@ -1,1 +1,136 @@
-# Place holder for app details
+require 'json'
+
+module MCollective
+  class Application
+
+    class Yum < MCollective::Application
+
+      description 'Interqact with the yum package manager'
+
+      # FIXME: complete the usage docs
+      usage <<-END_OF_USAGE
+mco yum [OPTIONS] [FILTERS] <ACTION> <PACKAGE>
+Usage: mco yum <PACKAGE> blah blah
+
+<finish off the docs>
+END_OF_USAGE
+
+      option :downloadonly,
+             description: "Don't update, just download",
+             arguments:   ['--downloadonly'],
+             type:        :bool
+
+      option :security,
+             description: 'Include security relevant packages',
+             arguments:   ['--security'],
+             type:        :bool
+
+      option :bugfixes,
+             description: 'Include bugfix relevant packages',
+             arguments:   ['--bugfixes'],
+             type:        :bool
+
+      option :cve,
+             description: 'Include packages needed to fix the given CVE',
+             arguments:   ['--cve CVE'],
+             type:        :array
+
+      option :bz,
+             description: 'Include packages needed to fix the given BZ',
+             arguments:   ['--bz BZ'],
+             type:        :array
+
+      option :'sec-severity',
+             description: 'Include security relevant packages, of this severity',
+             arguments:   ['--sec-severity SEVERITY'],
+             type:        :array
+
+      def handle_message(command, message, *args)
+        messages = {
+          1 => "Please specify a command. Valid commands are:\n%s",
+          2 => "Command must be one of:\n%s",
+          3 => "'%s' is not a valid package name"
+        }
+        send(command, messages[message] % args)
+      end
+
+      def post_option_parser(configuration)
+        valid_commands = %w(install downgrade remove reinstall list update check-update check_update clean update-minimal)
+
+        handle_message(:abort, 1, valid_commands.join(', ')) if ARGV.empty?
+        # the first arg should be a valid command
+        if valid_commands.include?(ARGV[0])
+          # it is, yay!
+          configuration[:command] = ARGV.shift
+          # process the rest of the args
+          ARGV[0..-1].each do |arg|
+            match_data = arg.match(/(.+)=(.+)/)
+            if match_data
+              arg_name = match_data[1].to_sym
+              arg_value = match_data[2]
+              if configuration.key? arg_name
+                configuration[arg_name] << arg_value
+              else
+                configuration[arg_name] = [arg_value]
+              end
+            elsif configuration.key? :packages
+              configuration[:packages] << arg
+            else
+              configuration[:packages] = [arg]
+            end
+          end
+        else
+          handle_message(:abort, 2, valid_commands.join(', '))
+        end
+      end
+
+      def validate_configuration(configuration)
+        if configuration.key? :packages
+          # make sure the package names are valid
+          configuration[:packages].each do |pkg|
+            # validate pkg with regex
+            handle_message(:abort, 3, pkg) unless pkg =~ /^[a-zA-Z\-_\.0-9\*]+$/
+          end
+        end
+
+        # flatten the packages, cve, bz, sec-severity arrays into strings
+        [:packages, :cve, :bz, :'sec-severity'].each do |param|
+          next unless configuration.key? param
+          unless configuration[param].empty?
+            configuration[param] = configuration[param].join(' ')
+          end
+        end
+        puts configuration.to_json
+      end
+
+      def main
+        # We have to change our process name in order to hide name of the
+        # service we are looking for from our execution arguments. Puppet
+        # provider will look at the process list for the name of the service
+        # it wants to manage and it might find us with our arguments there
+        # which is not what we really want ...
+        $0 = 'mco'
+
+        yum = rpcclient('yum')
+
+        command = configuration[:command]
+        configuration.delete :command
+
+        yum_result = yum.send(command, configuration)
+
+        if options[:output_format] == :json
+          puts yum_result.to_json
+          return
+        end
+
+        # FIXME: implement code to summarise and print the output
+        puts 'the output goes here (use --json for now)'
+
+        printrpcstats summarize: true, caption: 'yum %s results' % command
+        halt yum.stats
+      end
+
+    end
+
+  end
+end
